7ca2e52b5b093c4c48dd526070baba9520251a2e
Administrator <admin@example.com>
2021-03-06 03:22:15 +0900

    add splitlink

--- Front/front/src/index.tsx
+++ Front/front/src/index.tsx
@@ -6,19 +6,35 @@ import reportWebVitals from './reportWebVitals';
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloProvider
+  ApolloProvider,
+  HttpLink,
+  split
 } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { WebSocketLink } from "@apollo/client/link/ws";
 
-const link = new WebSocketLink({
+const httpLink = new HttpLink({ uri: 'https://localhost:5001/graphql/' });
+const wsLink = new WebSocketLink({
   uri: "wss://localhost:5001/graphql/",
   options: {
     reconnect: true
   }
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link,
+  link: splitLink,
   cache: new InMemoryCache()
 });
 
